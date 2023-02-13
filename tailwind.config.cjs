a1c0bf2e2ba632348be9ba30e772e7ca98a47bed
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-02-13 15:20:44 +0900

    edit: tailwind.config structure

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{tsx,ts,jsx,js}', './index.html'],
+  content: ["./src/**/*.{tsx,ts,jsx,js}", "./index.html"],
   theme: {
     extend: {},
-  },
-  colors: {
-    yellow: '#F4AE5C',
-    'light-gray': '#E0DED9',
-    gray: '#C8C8C8',
-    white: '#FFFFFF',
-    black: '#0D140D',
-    orange: '#E87540',
+    colors: {
+      yellow: "#F4AE5C",
+      "light-gray": "#E0DED9",
+      gray: "#C8C8C8",
+      white: "#FFFFFF",
+      black: "#0D140D",
+      orange: "#E87540",
+    },
   },
   plugins: [],
 };
