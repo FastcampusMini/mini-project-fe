d4dfafbec8b4d6c40df58bb6428d4edc8c0d502a
Wonny-ing <h.wonny11@gmail.com>
2023-02-13 21:55:08 +0900

    Feat: Create User page Markeup

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FaSearchDollar } from 'react-icons/fa';
+import { AiFillHome } from 'react-icons/ai';
+import { BsFillPersonFill } from 'react-icons/bs';
+
+const index = () => {
+  return (
+    <div>
+      <FaSearchDollar />
+      <AiFillHome />
+      <BsFillPersonFill />
+    </div>
+  );
+};
+
+export default index;
--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,10 +1,20 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { IoIosArrowForward } from 'react-icons/io';
 
 const User = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <div>User</div>
+      <div className='border w-[640px] min-w-[50%] m-auto h-[900px] px-10 py-10 relative'>
+        <h1 className='text-3xl mt-10 mb-5'>
+          안녕하세요! <span className='font-semibold'>홍혜원님</span>
+        </h1>
+        <div className='flex items-center gap-1 font-semibold text-gray' onClick={() => navigate('/signup')}>
+          <p>내 프로필 보기</p>
+          <IoIosArrowForward />
+        </div>
+      </div>
       <Outlet />
     </>
   );
