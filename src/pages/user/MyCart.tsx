4ab4740e415fdebca482bde6fb985be630afe655
1myeji <106291546+1myeji@users.noreply.github.com>
2023-02-13 23:08:55 +0900

    Fix: Add key props map 사용, key props 부여

--- src/pages/user/MyCart.tsx
+++ src/pages/user/MyCart.tsx
@@ -7,8 +7,8 @@ const Mycart = () => {
       <div className="w-10/12 m-auto">
         <h1 className="mb-5 text-2xl font-bold">장바구니</h1>
       </div>
-      {[1, 2, 3].map(() => (
-        <CartCard />
+      {[1, 2, 3].map((data, i) => (
+        <CartCard key={i} />
       ))}
     </article>
   );
