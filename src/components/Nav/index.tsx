42da1b948d1aa73057b2cd19828ada52c408a86d
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-02-18 23:35:04 +0900

    feat: userEdit validation

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,10 +1,45 @@
 import React from "react";
+import { IconType } from "react-icons";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
-const Nav = () => {
+import { useNavigate } from "react-router-dom";
+
+interface INavProps {
+  left?: "arrow" | "cancel";
+  right?: "arrow" | "cancel";
+}
+
+const ICON_SIZE = 28;
+
+const Nav = ({ left, right }: INavProps) => {
+  const navigate = useNavigate();
   return (
-    <nav className=" h-20 flex justify-between items-center px-7">
-      <AiOutlineArrowLeft size="36" className=" cursor-pointer" />
-      <AiOutlineArrowRight size="36" className="cursor-pointer" />
+    <nav className='mb-10 flex justify-between px-7'>
+      {left === "arrow" ? (
+        <AiOutlineArrowLeft
+          size={ICON_SIZE}
+          className='cursor-pointer'
+          onClick={() => navigate(-1)}
+        />
+      ) : left === "cancel" ? (
+        <span className='font-bold text-lg' onClick={() => navigate(-1)}>
+          취소
+        </span>
+      ) : (
+        <div></div>
+      )}
+      {right === "arrow" ? (
+        <AiOutlineArrowRight
+          size={ICON_SIZE}
+          className='cursor-pointer'
+          onClick={() => navigate(1)}
+        />
+      ) : right === "cancel" ? (
+        <span className='font-bold text-lg' onClick={() => navigate(-1)}>
+          취소
+        </span>
+      ) : (
+        <div></div>
+      )}
     </nav>
   );
 };
