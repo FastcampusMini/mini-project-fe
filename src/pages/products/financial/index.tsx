5f7407ba5761c1bfb5664b2c932781480e535806
hyunsooBae <hyunsoobae.dev@gmail.com>
2023-02-20 03:14:38 +0900

    feat: Modify financial & financial detail pages
    - 검색 필터 추가(수정중)
    - 사용자가 가입 가능한 상품만 보기 체크 여부 추가(수정중)
    - 태그로 검색 기능 추가(수정중)
    - 모달 컴포넌트 적용 및 장바구니 버튼 추가
    - 스타일 일부 수정

--- src/pages/products/financial/index.tsx
+++ src/pages/products/financial/index.tsx
@@ -1,50 +1,121 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import axios from 'axios'
 import { CgSearch } from 'react-icons/cg'
 import ProductCard from "../../../components/FinancialProdCard";
 
 const Financial = () => {
-  const [ isActive, setIsActive ] = useState(false)
-  const [ searchWord, setSearchWord ] = useState('')
+  // name : {...register("해당inputName")}, handleSubmit : onsubmit={handleSubmit(onSubmit)}
+  // const {register, handleSubmit} = useForm()
+  const [keyword, setKeyword] = useState([])
+  const [searchName, setSearchName] = useState('')
+  const [products, setProducts] = useState([])
+  const [page, setPage] = useState()
 
-  const btnActive = () => {
-    setIsActive(!isActive)
+  useEffect(() => {
+    getSearchResult(keyword)
+  }, [keyword])
+    
+  const getSearchResult = async (keyword) => {
+    const page = 1
+    const BASEURI = `http://52.78.32.230:8080/search?name=${keyword}&page=${page}`
+    const res = await axios(BASEURI)
+    setProducts(res.data.data.content)
+    console.log(BASEURI)
   }
+  
+  const addKeyword = (e) => {
+    const clicked = e.target.value
+    // let newKeyword = []
+    // newKeyword = keyword.includes(clicked) ? keyword.filter((item) => item != clicked) : [...keyword, clicked]
+    keyword.indexOf(clicked) > -1 
+    ? setKeyword(keyword.filter((item) => item != clicked))
+    : setKeyword([...keyword, clicked])
+    // setKeyword(newKeyword)
+  }  
 
   return (
     <div>
-      <h2 className="my-8 text-3xl font-bold">상품 검색</h2>
+      <h2 className="mt-8 sm:mb-8 text-3xl font-bold">상품 검색</h2>
       <div className="mb-16">
+        <form className="flex flex-wrap gap-3 rounded-[10px]">
+          <div className="w-full sm:text-right text-left">
+            <label htmlFor="check1" className="">내가 가입할 수 있는 상품만 보기</label>
+            <input type="checkbox" id='check1' className="ml-2" />
+          </div>
 
-        <form className="relative flex rounded-[10px] border border-light-gray bg-white overflow-hidden focus:outline-none focus-within:border-yellow focus-within:border-2 valid:border-2 valid:border-yellow">
+          <div className="pr-3 py-2 outline-none rounded-full border-2 border-light-gray bg-white overflow-hidden focus:outline-none focus:border-2 hover:border-2 focus:border-yellow invalid:border-light-gray valid:border-yellow hover:border-yellow text-lg">
+            <select className="pl-3 outline-none focus:outline-none">
+              <option hidden>상품 유형</option>
+              <option value={''}>전체</option>
+              <option value={''}>대출</option>
+              <option value={''}>예금</option>
+            </select>
+          </div>
 
-          <select className="px-4 outline-none">
-            <option>전체</option>
-            <option>대출</option>
-            <option>예금</option>
-          </select>
+          <div className="pr-3 outline-none rounded-full border-2 border-light-gray bg-white overflow-hidden focus:outline-none focus:border-2 hover:border-2 focus:border-yellow invalid:border-light-gray valid:border-yellow hover:border-yellow text-lg">
+            <select className="pl-3 py-2 outline-none focus:outline-none">
+              <option hidden>연봉</option>
+              <option value={''}>전체</option>
+              <option value={''}>2000 미만</option>
+              <option value={''}>3000 미만</option>
+              <option value={''}>4000 미만</option>
+              <option value={''}>5000 이상</option>
+            </select>
+          </div>
 
-          <input type='text' required className="w-full p-4 pr-16 appearance-none autofill:valid:bg-white focus:border-none active:border-none border-transparent text-xl outline-none" 
-          placeholder="원하는 상품을 검색하세요" 
-          onChange={(e) => setSearchWord(e.target.value)} />
-          <button className="absolute top-0 bottom-0 right-4 text-black40" type='submit'><CgSearch size='34'></CgSearch></button>
+          <div className="relative grow">
+            <input type='text' required className="pl-4 pr-4 py-3 w-full h-full rounded-full border-2 border-light-gray bg-white overflow-hidden focus:outline-none focus-within:border-yellow focus-within:border-2 valid:border-2 valid:border-yellow" 
+            placeholder="원하는 상품을 검색하세요" 
+            defaultValue={''}
+            onChange={(e) => setSearchName(e.target.value)} />
+            <button className="absolute top-0 bottom-0 right-4 text-black40" type='submit'><CgSearch size='26'></CgSearch></button>
+          </div>
+
         </form>
       </div>
 
-      <h2 className='my-8 text-3xl font-bold'>맞춤 필터링</h2>
-      <div className="flex gap-5 mb-16">
-        <button className={`aspect-square w-20 border rounded-[10px] text-xl bg-yellow border-yellow text-white font-bold`} onClick={btnActive}>전체</button>
-        <button className='aspect-square w-20 rounded-[10px] border border-black5 bg-black5 text-xl'>대출</button>
-        <button className='aspect-square w-20 rounded-[10px] border border-black5 bg-black5 text-xl'>예금</button>
+      <h2 className='my-8 text-3xl font-bold'>키워드로 찾기</h2>
+      <div className="mb-16 flex flex-wrap gap-3">
+        {['대출', '소액', '세테크', '부동산'].map((data, i) => (
+          // i !== 0 
+          // ? <Button key={i} data={data} addKeyword={addKeyword} isOn={false} /> 
+          // : <Button key={i} data={data} addKeyword={addKeyword} isOn={true} />
+          <Button key={i} data={data} addKeyword={addKeyword} isOn={false} />
+        ))}
       </div>
 
-      <div className="mb-16">
-        {/* <div className="my-10 flex justify-center items-center text-black40 font-bold text-lg">등록된 상품이 없습니다.</div> */}
-        <ProductCard />
-        <ProductCard />
+      <div className="mb-16">        
+        {products.length > 0
+        ? products.map((data) => (
+          <ProductCard key={data.productId} data={data} />
+        )) 
+        : 
+        <div className="my-40 flex justify-center items-center text-black40 font-bold text-lg">등록된 상품이 없습니다.</div>
+        }
       </div>
     </div>
   );
 };
+
+const Button = ({ data, addKeyword, isOn }) => {
+  const [ isActive, setIsActive ] = useState(isOn)
+  const btnToggle = (e) => {    
+    setIsActive((prev) => !prev)
+    addKeyword(e)
+  }
+  return (
+    <button
+      className={`px-6 py-2 rounded-full text-lg text-black40
+        ${ isActive 
+          ? 'border-2 border-yellow text-yellow font-bold' 
+          : 'border border-light-gray'}`}
+      onClick={btnToggle}
+      value={data}
+      >
+      {data}
+    </button>
+  )
+}
 
 export default Financial;
