65fc7a465fcd74f6874816633a29208c152f38ee
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-02-22 12:15:58 +0900

    fix) set token cacheTime to Infinity

--- src/libs/hooks/useLogin.tsx
+++ src/libs/hooks/useLogin.tsx
@@ -10,7 +10,7 @@ const useLogin = (options?) => {
     (loginInfo: ILoginInput) => ax.postLogin(loginInfo),
     Object.assign(
       {
-        cacheTime: 1000,
+        cacheTime: Infinity,
         onSuccess: (data) => {
           queryClient.setQueryData(['token'], () => data);
         },
