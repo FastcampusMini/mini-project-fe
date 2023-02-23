bc703676f9c811291884381348a193b1d531d2cf
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-02-24 00:43:44 +0900

    feat: design main page and add user-info-edit-feature

---
+++ src/components/LoanProductCard copy.tsx
@@ -0,0 +1,51 @@
+import { ax } from '@/libs/axiosClient';
+import { useMutation } from '@tanstack/react-query';
+import React from 'react';
+import { BiChevronRight } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
+
+interface LoanProductCardProps {
+  product: IProduct;
+}
+
+const LoanProductCard = ({ product }: LoanProductCardProps) => {
+  return (
+    <>
+      <Link to={`/products/financial/${product?.productId}`}>
+        <div className='w-full h-auto shadow-md rounded-lg border border-black/10 hover:scale-95 transition-all'>
+          <div className='flex h-36 justify-between px-4'>
+            <div className='flex items-center'>
+              {product?.logo ? (
+                <img src={product?.logo} className='w-8 h-8' />
+              ) : (
+                <div className='w-8 h-8 rounded-full bg-gray ml-4'></div>
+              )}
+              <div className='flex flex-col gap-1 mx-4'>
+                <span className='font-semibold text-black/50'>
+                  {product?.brand}
+                </span>
+                <h4 className='font-bold text-3xl '>{product?.name}</h4>
+              </div>
+            </div>
+            <div className='flex flex-col font-bold text-orange items-end justify-center gap-2 mx-4 text-lg'>
+              <span>최저 {product?.rate} %</span>
+              <span>{product?.price} 만 원</span>
+            </div>
+          </div>
+
+          <div className='flex bg-black/5 px-5 justify-between h-14 items-center'>
+            <span className='font-semibold text-black/60 text-lg'>
+              {product?.detail}
+            </span>
+            <BiChevronRight
+              size='30'
+              className='text-black/60 cursor-pointer'
+            />
+          </div>
+        </div>
+      </Link>
+    </>
+  );
+};
+
+export default LoanProductCard;
--- src/pages/main/SlideCard.tsx
+++ src/pages/main/SlideCard.tsx
@@ -10,57 +10,57 @@ import sticker7 from '@/assets/stickers/free-sticker7.png';
 import sticker8 from '@/assets/stickers/free-sticker8.png';
 import sticker9 from '@/assets/stickers/free-sticker9.png';
 import { MdRecommend } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
 interface IProps {
   product: IProduct;
 }
-function stickers(num: number) {
-  const x = `${num}`;
-  switch (x[x.length - 1]) {
-    case '0':
-      return sticker0;
-    case '1':
-      return sticker1;
-    case '2':
-      return sticker2;
-    case '3':
-      return sticker3;
-    case '4':
-      return sticker4;
-    case '5':
-      return sticker5;
-    case '6':
-      return sticker6;
-    case '7':
-      return sticker7;
-    case '8':
-      return sticker8;
-    case '9':
-      return sticker9;
-    default:
-      return sticker0;
-  }
-}
+const getSticker = (num: number) => {
+  const _stickers = [
+    sticker0,
+    sticker1,
+    sticker2,
+    sticker3,
+    sticker4,
+    sticker5,
+    sticker6,
+    sticker7,
+    sticker8,
+    sticker9,
+  ];
+  const index = num % 10;
+  return _stickers[index];
+};
+
 const SlideCard = ({ product }: IProps) => {
+  const navigate = useNavigate();
   return (
-    <div className='rounded-2xl bg-[#fff] w-64 h-96 flex flex-col justify-between p-3 shadow-lg shrink-0'>
+    <div
+      className='cursor-pointer rounded-2xl bg-[#fff] w-64 h-96 flex flex-col justify-between p-3 shadow-lg shrink-0 first:ml-10 hover:-translate-y-3 transition-transform'
+      onClick={() => navigate(`/products/financial/${product?.productId}`)}>
       <div className='w-20 h-10 rounded-full'>
-        <img src={product.logo} alt='' className='w-8 h-8' />
+        <img src={product?.logo} alt='' className='w-8 h-8' />
       </div>
-      <img
-        className=' h-36 relative object-contain'
-        src={stickers(product.productId)}
-      />
+
+      <div className='relative rounded-full w-28 h-28 mx-auto flex items-center justify-center bg-light-gray'>
+        <div className='absolute w-36'>
+          <img
+            className='relative w-36 mx-auto'
+            src={getSticker(product?.productId)}
+          />
+        </div>
+      </div>
+
       <div className='flex flex-col gap-4'>
-        <h3 className='font-bold text-2lg'>{product.name}</h3>
+        <h3 className='font-bold text-2lg'>{product?.name}</h3>
         <p className='text-sm text-black60'>
           긴급자금이 필요하세요? 고금리 대출이자로 고민하시나요? 당신의 희망을
           이루도록 돕겠습니다!
         </p>
         <div className='flex items-end justify-end gap-2'>
-          <span className='font-semibold text-yellow'>{product.rate}%</span>
+          <span className='font-semibold text-yellow'>{product?.rate}%</span>
           <span className='font-bold text-xl text-orange'>
             {' '}
-            ₩{product.price}만
+            ₩{product?.price}만
           </span>
         </div>
       </div>
