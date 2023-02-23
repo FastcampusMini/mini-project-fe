b4c48d2c15fa1af24d9b8ea859397b3102c1edcd
hyunsooBae <hyunsoobae.dev@gmail.com>
2023-02-24 03:47:22 +0900

    design: Modify Css Styles

--- src/components/ui/Navigation/index.tsx
+++ src/components/ui/Navigation/index.tsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 const index = () => {
   const navigate = useNavigate();
   return (
-    <div className='flex justify-around items-center text-4xl text-gray py-6 rounded-t-3xl border-t border-black/20 cursor-pointer bottom-0 absolute w-full left-0'>
+    <div className='flex justify-around items-center text-4xl text-gray py-6 rounded-t-3xl border-t border-black/20 cursor-pointer bottom-0 absolute w-full left-0 bg-white'>
       <FaSearchDollar className='hover:text-yellow' onClick={() => navigate('/products/financial')} />
       <AiFillHome className='hover:text-yellow' onClick={() => navigate('/main')} />
       <BsFillPersonFill className='hover:text-yellow' onClick={() => navigate('/user')} />
