746823adf6f668544f8525482b441e4e1a47c346
hyunsooBae <hyunsoobae.dev@gmail.com>
2023-02-25 20:42:54 +0900

    fix: Add search checkbox event
    - 검색 화면에서 '내가 가입할 수 있는 상품만 보기' 체크시 바로 검색 요청
      보냄
    - 검색 응답 데이터 없을 때 '검색 결과가 없습니다' 안내 문구 추가
    - 검색 화면에서 예상금리 7.7%로 고정되서 나오던 내용 수정
    - 검색 화면에도 메인의 scrollToTop 버튼 추가

--- src/components/FinancialProdCard.tsx
+++ src/components/FinancialProdCard.tsx
@@ -7,10 +7,9 @@ const FinancialProdCard = ({ data }) => {
     <Link to={`/products/financial/${data.productId}`} className='flex items-center py-8 shadow-md rounded-b-lg rounded-t-3xl border border-black/10 hover:scale-95 transition-all bg-white px-8 mt-6'>
       <img src={data.logo} className="h-20 grow" />
       <div className='w-full text-right'>
-        {/* <span className='text-black40'>{data.brand}</span> */}
         <h3 className="font-bold text-black80 text-2xl">{data.name}</h3>
-        <b className="font-semibold text-yellow"><span className='text-black40 text-sm'>예상금리 </span>7.7%</b>
-        <b className="font-semibold text-lg text-orange"><span className='ml-6 text-black40 text-sm'>최대한도</span> ₩ {(data.price).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 만</b>
+        <b className="font-semibold text-yellow"><span className='text-black40 text-sm'>예상금리</span> {data.rate}%</b>
+        <b className="font-semibold text-lg text-orange"><span className='ml-6 text-black40 text-sm'>최대한도</span> ₩{(data.price).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 만</b>
       </div>
     </Link>
   )
